8899ad0496393ddd355ae434ce51ec16b5282136
zhongxl <zhongxl@ctrip.com>
2016-11-18 15:08:30 +0800

    Initial commit

---
+++ src/main/scala/ScalaLearnOne.scala
@@ -0,0 +1,29 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by zhongxl on 2016/11/17.
+  */
+
+object ScalaLearnOne{
+  def main(args: Array[String]) {
+
+    // 声明
+
+    val spark=SparkSession.builder().master("local")
+      .appName("spark_mllib")
+      .config("spark.sql.warehouse.dir","file:///")
+      .getOrCreate()
+
+    val sqlContext = spark
+
+
+    val adress="D:/project_csm/bigtable_2B_xc_new.csv"
+    val df= sqlContext.read.format("csv")
+      .option("header","true")
+      .option("inferSchema","true")
+      .load(adress)
+    df.show()
+  }
+}
