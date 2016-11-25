9b2c6fe7d4651b716b176220c8e86d89221e0d37
zhongxl <zhongxl@ctrip.com>
2016-11-25 17:03:11 +0800

    更新pom 和ide 设置 通过箱须处理异常值 添加自带 交叉验证部分的案例 添加异常值处理中的正态分布数据的处理 添加异常值处理中的只过滤95分位的值得方法

--- src/main/scala/ScalaLearnOne.scala
+++ src/main/scala/ScalaLearnOne.scala
@@ -1,8 +1,8 @@
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.clustering.KMeans
 import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 /**
   * Created by zhongxl on 2016/11/17.
@@ -14,7 +14,8 @@ object ScalaLearnOne {
     // 声明
 
     val spark = SparkSession.builder.master("local")
-      .appName("spark_ml").config("spark.sql.warehouse.dir", "file:///")
+      .appName("spark_ml")
+//      .config("spark.sql.warehouse.dir", "file:///")
       .getOrCreate()
 
 
@@ -34,16 +35,18 @@ object ScalaLearnOne {
       val vectorAssembler = new VectorAssembler()
         .setInputCols(args)
         .setOutputCol("features")
-      val kMeans = new KMeans().setK(50).setFeaturesCol("features").setPredictionCol(flag + "km")
+      val kMeans = new KMeans().setK(50).setFeaturesCol("features").setPredictionCol(flag + "_km")
       val pipeline = new Pipeline().setStages(Array(vectorAssembler, kMeans))
       case class Temp(flag: Double)
-      val model = pipeline.fit(oriData) // 忽略这个告警
-      val data = model.transform(oriData).drop("features").drop(flag) //忽略此告警
+      val model = pipeline.fit(oriData)
+      val data = model.transform(oriData).drop("features").drop(flag)
       return data
     }
 
     val data = kMeansDiscreteData(df, "uid_grade")
     data.show()
+
+
     val s = data.stat.approxQuantile("uid_age", Array(0.25, 0.5, 0.75), 0.2)
     println(s.toList)
     val q1 = s(0)
@@ -66,40 +69,86 @@ object ScalaLearnOne {
     def outlierDataProcess(dataFrame: DataFrame): DataFrame = {
       var newdata = dataFrame
       for (flag <- dataFrame.columns.toList) {
-        val quantiles = dataFrame.select(flag).stat.approxQuantile(flag, Array(0.25, 0.5, 0.75), 0.1)
+        val quantiles = dataFrame.select(flag)
+          .stat.approxQuantile(flag, Array(0.25, 0.5, 0.75), 0.1)
         val q1 = quantiles(0)
         val median = quantiles(1)
         val q3 = quantiles(2)
         val iqr = q3 - q1
         val lowerRange = q1 - 1.5 * iqr
         val upperRange = q3 + 1.5 * iqr
-        //        println("FOLLOWS ARE THE OUTLIER DATA OF " + flag)
-        //        dataFrame.select(flag).filter(s"$flag<$lowerRange or $flag > $upperRange").show(15)
+        println("FOLLOWS ARE THE OUTLIER DATA OF " + flag)
+        dataFrame.select(flag).filter(s"$flag<$lowerRange or $flag > $upperRange").show(3)
         val compare = udf { (row: Int) =>
-          if (row > upperRange || row < lowerRange) {
-            Math.floor(median) // 向下取整数
-          }
-          else {
-            row //注意这里不能写成return
-          }
+          if (row > upperRange || row < lowerRange) {Math.floor(median)}
+          else {row}
         }
         val juge = udf { (row: Int) =>
-          if (row == -1) {
-            0
-          } // 业务上未填写即为-1
-          else {
-            1
-          }
-        }
+          if (row == -1) {0}
+          else {1}}
         newdata = newdata.withColumn(flag + "_new", compare(col(flag)))
           .withColumn(flag + "TF", juge(col(flag))).drop(flag)
         //注意这种更新DataFrame方式
       }
       return newdata
-
     }
+
     println("-" * 50)
     outlierDataProcess(df.select("uid_age", "uid_grade")).show(5)
+
+    def normalDataProcess(dataFrame: DataFrame): DataFrame = {
+      var newData = dataFrame
+      for (flag <- dataFrame.columns.toList) {
+        val describes = dataFrame.describe(flag)
+        //        describes.show()
+        val meanVal = describes.take(3)(1).toSeq(1).toString.toDouble
+        val stdVal = describes.take(3)(2).toSeq(1).toString.toDouble
+        val maxVal = meanVal + 3 * stdVal
+        val minVal = meanVal - 3 * stdVal
+        val compare = udf { (row: Int) =>
+          if (row > maxVal || row < minVal) {Math.floor(meanVal)}
+          else {row}}
+        val juge = udf { (row: Int) =>
+          if (row == -1) {0}
+          else {1}}
+        newData = newData
+          .withColumn(flag + "_new", compare(col(flag))).drop(flag)
+      }
+      return newData
+    }
+
+    normalDataProcess(df.select("uid_age", "uid_grade")).show(5)
+
+    def outliersProcess(dataFrame: DataFrame, replace: Int = 95): DataFrame = {
+      var newData = dataFrame
+      for (flag <- dataFrame.columns.toList) {
+        val quantiles = dataFrame.select(flag)
+          .stat.approxQuantile(flag, Array(0.5, 0.95), 0.1)
+        val q95 = quantiles(1)
+        val q5 = quantiles(0)
+        val compare95 = udf { (row: Int) =>
+          if (row > q95) {Math.floor(q95)}
+          else if (row == -1) {q5}
+          else {row}}
+        val compare5 = udf { (row: Int) =>
+          if (row > q95) {Math.floor(q5)}
+          else if (row == -1) {q5}
+          else {row}}
+
+        if (replace == 5) {
+          newData = newData.withColumn(flag + "_new", compare5(col(flag))).drop(flag)
+        }
+        else {
+          newData = newData.withColumn(flag + "_new", compare95(col(flag))).drop(flag)
+        }
+      }
+      return newData
+    }
+
+    outliersProcess(df.select("uid_age", "uid_grade")).show(5)
+
+
+
 
   }
 }
